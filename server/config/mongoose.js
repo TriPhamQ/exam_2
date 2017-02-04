a7e5499f46f2c816e719e0440aa1fe2242d252b6
Tri Pham <phamqtri1992@gmail.com>
2017-02-04 11:41:48 -0800

    changed db

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose'),
 	path = require('path'),
 	models_path = path.join(__dirname, "../models"),
 	reg = new RegExp(".js$", "i"),
-	dbURI = 'mongodb://localhost/rushhour_db';
+	dbURI = 'mongodb://localhost/exam2_db';
 
 mongoose.Promise = global.Promise;
 
